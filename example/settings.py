9c279ab0a99db1df1d9e4b5c678d6e2c3367363a
kehlin swain <kehlinswain@kehlins-MacBook-Pro.local>
2018-07-05 19:01:25 -0500

    update of static files

--- example/settings.py
+++ example/settings.py
@@ -132,5 +132,5 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'djangoexample/staticfiles'),
 )
