74f96c1dd5e99f96714158754423831c4deb3133
kehlin swain <kehlinswain@kehlins-MacBook-Pro.local>
2018-07-09 14:57:11 -0500

    updates to static pt 4

--- example/settings.py
+++ example/settings.py
@@ -130,12 +130,3 @@ USE_TZ = True
 # BASE_DIR = os.path.dirname(os.path.abspath('staticfiles'))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'))
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
